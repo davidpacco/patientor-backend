7af6292affd88775e313044c93e782ef5d4cce6e
davidpacco <david.j.pacco@gmail.com>
2024-09-03 17:03:36 -0500

    Add exercise 9.9

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
